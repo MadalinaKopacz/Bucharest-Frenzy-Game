27cef34d9eb9bf6ad6c481fe9b70ae6997e6db18
matei_nastase <nastasematei@gmail.com>
2023-01-14 23:57:32 +0200

    Game balance

--- Game Dev/Assets/Scripts/Player/PlayerScript.cs
+++ Game Dev/Assets/Scripts/Player/PlayerScript.cs
@@ -131,7 +131,7 @@ public class PlayerScript : MonoBehaviour
         {
             Destroy(collision.gameObject);
             Inverted = true;
-            yield return new WaitForSeconds(5);
+            yield return new WaitForSeconds(7);
             Inverted = false;
 
         }
